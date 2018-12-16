7eddb92065cadfc5e94b2f7394aee3c0cb9b9b63
Vonds <vonds13@gmail.com>
2018-12-16 15:48:56 -0500

    dogs code challenge

---
+++ dogs.js
@@ -0,0 +1,23 @@
+// TODO: define the Dog class here
+class Dog {
+    constructor(name, species, size) {
+        this.name = name;
+        this.species = species;
+        this.size = size;
+    }
+    bark() {
+        if (this.size > 60) {
+            return 'GRR! GRR!';
+        } else {
+            return 'Woof! Woof!';
+        }
+    }
+}
+
+const fang = new Dog("Fang", "boardhound", 75);
+console.log(`{${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+
+const snowy = new Dog("Snowy", "terrier", 22);
+console.log(`{${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
