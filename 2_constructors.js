7799b5969c1e7e88bfc4ae3508fcab29eaba8dd1
Vonds <vonds13@gmail.com>
2018-12-16 21:09:37 -0500

    constructors lesson

---
+++ 2_constructors.js
@@ -0,0 +1,16 @@
+// Constructor 
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.getSummary = function () {
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    }
+}
+
+// Instantiate an Object
+const book1 = new Book('Book One', 'Sarah Olamina', '2013');
+const book2 = new Book('Book two', 'Kendra Gold', '2016');
+
+console.log(book1.getSummary());
+console.log(book2.getSummary());
