b95337a19d4b512bbb8d3fece1e3505879a8ffba
Vonds <vonds13@gmail.com>
2018-12-17 09:05:32 -0500

    prototype lesson

--- 3_prototypes.js
+++ 3_prototypes.js
@@ -10,8 +10,22 @@ Book.prototype.getSummary = function () {
     return `${this.title} was written by ${this.author} in ${this.year}`;
 }
 
+// getAge
+Book.prototype.getAge = function () {
+    const years = new Date().getFullYear() - this.year;
+    return `${this.title} is ${years} years old`;
+};
+
+// Revise/ Change year
+Book.prototype.revise = function (newYear) {
+    this.year = newYear;
+    this.revised = true;
+}
+
 // Instantiate an Object
 const book1 = new Book('Book One', 'Sarah Olamina', '2013');
 const book2 = new Book('Book two', 'Kendra Gold', '2016');
 
+console.log(book2);
+book2.revise('2018');
 console.log(book2);
